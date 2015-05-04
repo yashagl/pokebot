b22b2066c82959a76bd24beebc7e9a212054dcc3
Morfent <morfentdoge@gmail.com>
2015-05-04 06:35:09 -0400

    Update usage stats link

--- commands.js
+++ commands.js
@@ -474,7 +474,7 @@ exports.commands = {
 	usage: 'usagestats',
 	usagestats: function (arg, user, room) {
 		var text = (room === user || user.canUse('usagestats', room)) ? '' : '/pm ' + user.id + ', ';
-		text += 'http://www.smogon.com/stats/2015-03/';
+		text += 'http://www.smogon.com/stats/2015-04/';
 		this.say(room, text);
 	},
 	seen: function (arg, user, room) { // this command is still a bit buggy
